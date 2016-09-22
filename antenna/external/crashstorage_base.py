6423c7b853b6d6b6bf748b908e07f2c9a5f9dbe2
Will Kahn-Greene <willkg@mozilla.com>
2016-09-22 13:35:15 -0400

    Enhance NoOpCrashStore to remember the last 10 crashes
    This enhances the NoOpCrashStore to remember the last 10 crashes which
    will make it useful for testing.

--- antenna/external/crashstorage_base.py
+++ antenna/external/crashstorage_base.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import logging
 
 
@@ -15,6 +19,23 @@ class CrashStorageBase:
 
 
 class NoOpCrashStorage(CrashStorageBase):
-    """This is a no-op crash storage that just logs that it could have saved a crash"""
+    """This is a no-op crash storage that logs crashes it would have stored
+
+    It keeps track of the last 10 crashes in ``.crashes`` with the most
+    recently stored crash at the end of the list.
+
+    """
+    def __init__(self, config):
+        super().__init__(config)
+        self.crashes = []
+
+    def add_crash(self, raw_crash, dumps, crash_id):
+        self.crashes.append(
+            (raw_crash, dumps, crash_id)
+        )
+        # Nix all but the last 10 crashes
+        self.crashes = self.crashes[-10:]
+
     def save_raw_crash(self, raw_crash, dumps, crash_id):
         logger.info('crash no-op: %s %s %s', crash_id, raw_crash, dumps.keys())
+        self.add_crash(raw_crash, dumps, crash_id)
