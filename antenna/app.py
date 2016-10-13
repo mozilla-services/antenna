0faceed071805ea1bbc2ea218f43c544e94b5707
Will Kahn-Greene <willkg@mozilla.com>
2016-10-12 21:45:28 -0400

    Fix logging datefmt
    We should use %S instead of %s.

--- antenna/app.py
+++ antenna/app.py
@@ -94,7 +94,7 @@ def setup_logging(config):
         'formatters': {
             'development': {
                 'format': '[%(asctime)s] [%(levelname)s] %(name)s: %(message)s',
-                'datefmt': '%Y-%m-%d %H:%M:%s %z',
+                'datefmt': '%Y-%m-%d %H:%M:%S %z',
             },
         },
         'handlers': {
