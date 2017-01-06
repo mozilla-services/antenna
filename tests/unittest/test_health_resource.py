f9e0d3270359840db142c9781e18eee888f9ff3b
Will Kahn-Greene <willkg@mozilla.com>
2017-01-05 21:46:17 -0500

    Fix unintersting flake8 issues

--- tests/unittest/test_health_resource.py
+++ tests/unittest/test_health_resource.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 class TestHealthChecks:
     def test_no_version(self, client, tmpdir):
         # Set basedir here to tmpdir which we *know* doesn't have a
