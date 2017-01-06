86d824f97a4b7b9301bca89a4014231c0f3492a1
Will Kahn-Greene <willkg@mozilla.com>
2017-01-05 21:04:03 -0500

    Add missing license headers

--- tests/systemtest/conftest.py
+++ tests/systemtest/conftest.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 import json
 import logging
 import os
@@ -8,6 +12,7 @@ import boto3
 from botocore.client import Config
 from everett.manager import ConfigManager, ConfigEnvFileEnv, ConfigOSEnv
 import pytest
+
 
 # Add repository root so we can import testlib.
 REPO_ROOT = Path(__file__).parent.parent.parent
