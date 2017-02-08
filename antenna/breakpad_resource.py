e33af6e36607a4af89a74df299d3296930a0a525
Will Kahn-Greene <willkg@mozilla.com>
2017-02-08 16:45:32 -0500

    Comment out last change and add debugging logging

--- antenna/breakpad_resource.py
+++ antenna/breakpad_resource.py
@@ -317,6 +317,8 @@ class BreakpadSubmitterResource(RequiredConfigMixin):
     def save_crash_to_storage(self, raw_crash, dumps, crash_id):
         """Saves the crash to storage"""
 
+        logger.info('%s saving now...', crash_id)
+
         # FIXME(willkg): How to deal with errors here? These should handle
         # retrying, so if it bubbles up to this point, then it's an unretryable
         # unhandleable error.
@@ -338,8 +340,9 @@ class BreakpadSubmitterResource(RequiredConfigMixin):
         #
         # NOTE(willkg): time.time returns seconds, but .timing() wants
         # milliseconds, so we multiply!
-        delta = (time.time() - raw_crash['timestamp']) * 1000
-        self.mymetrics.timing('crash_handling.time', delta)
+        # FIXME(willkg): temporarily backing these out because something's not working
+        # delta = (time.time() - raw_crash['timestamp']) * 1000
+        # self.mymetrics.timing('crash_handling.time', delta)
         logger.info('%s saved', crash_id)
 
     def join_pool(self):
