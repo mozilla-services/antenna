c1b902c550f6a42fd7668f9709416651c5b959ac
Will Kahn-Greene <willkg@mozilla.com>
2019-03-06 14:27:33 -0500

    bug 1527343: rewrite publishing using synchronous "batch"
    This redoes publishing to Pub/Sub by implementing a new SynchronousBatch that
    doesn't do any batching at all and instead publishes crash ids immediately in
    the same execution context. This allows any exceptions from the library to
    bubble up and get handled correctly. It also minimizes the total time to handle
    a crash from end to end.

--- antenna/ext/pubsub/crashpublish.py
+++ antenna/ext/pubsub/crashpublish.py
@@ -6,6 +6,8 @@ import logging
 
 from everett.component import ConfigOptions
 from google.cloud import pubsub_v1
+from google.cloud.pubsub_v1.publisher import futures
+from google.cloud.pubsub_v1.types import PubsubMessage
 
 from antenna.ext.crashpublish_base import CrashPublishBase
 from antenna.heartbeat import register_for_verification
@@ -13,13 +15,37 @@ from antenna.heartbeat import register_for_verification
 
 logger = logging.getLogger(__name__)
 
-# This is the maximum time in seconds that a batch will sit around for before
-# getting published. Since .pubish_crash() is synchronous (it blocks on the
-# result), I think we want this short-ish. But it's nice to batch publishing.
-#
-# NOTE(willkg): Maybe think about making this configurable so we can find a
-# more optimal value? Maybe calculate it based on concurrant crashmovers value?
-BATCH_MAX_LATENCY = 0.5
+
+class SynchronousBatch:
+    """Synchronous batch class.
+
+    Instead of batching published messages and then spinning off a thread to
+    send them as a batch at some point later, this sends messages as they are
+    published. In this way, any exceptions thrown while publishing bubble up to
+    the right place in Antenna and get handled correctly.
+
+    """
+
+    def __init__(self, client, topic, settings, autocommit=True):
+        self._client = client
+        self._topic = topic
+        self._settings = settings
+
+    def publish(self, message):
+        """Publish message.
+
+        This publishes messages immediately--it does no batching and requires
+        no threads.
+
+        """
+        if not isinstance(message, PubsubMessage):
+            message = PubsubMessage(**message)
+
+        # Publish and return result wrapped in future
+        future = futures.Future()
+        resp = self._client.api.publish(self._topic, [message], timeout=5)
+        future.set_result(resp.message_ids[0])
+        return future
 
 
 class PubSubCrashPublish(CrashPublishBase):
@@ -73,28 +99,19 @@ class PubSubCrashPublish(CrashPublishBase):
         self.project_id = self.config('project_id')
         self.topic_name = self.config('topic_name')
 
-        # Batches crash_ids for at most BATCH_MAX_LATENCY seconds
-        self.batch_settings = pubsub_v1.types.BatchSettings(max_latency=BATCH_MAX_LATENCY)
-        self.publisher = pubsub_v1.PublisherClient(self.batch_settings)
+        self.publisher = pubsub_v1.PublisherClient()
+        self.publisher._batch_class = SynchronousBatch
         self.topic_path = self.publisher.topic_path(self.project_id, self.topic_name)
 
         register_for_verification(self.verify_topic)
 
     def verify_topic(self):
-        """Verify topic exists and can be viewed."""
-        # Publish a fake crash id
+        """Verify topic can be published to by publishing fake crash id."""
         future = self.publisher.publish(self.topic_path, data=b'test')
-
-        # This will block until the crash has been published; if it publishes
-        # we're good to go
         future.result()
 
     def publish_crash(self, crash_id):
         """Publish a crash id to a Pub/Sub topic."""
         data = crash_id.encode('utf-8')
         future = self.publisher.publish(self.topic_path, data=data)
-
-        # This forces publishing to be synchronous so if there are problems,
-        # this will raise an exception and that'll get handled by the retry
-        # logic.
         future.result()
