9cfe5785ee31a77740fc5b462eda8d61bfd9324d
Sven Marnach <sven@mozilla.com>
2023-10-18 15:34:34 +0200

    Fix script to send example crash report.

--- bin/send_crash_report.sh
+++ bin/send_crash_report.sh
@@ -5,12 +5,7 @@
 
 set -euo pipefail
 
-HOST=$1
-if [ -z "${HOST}" ]; then
-    HOST="http://localhost:8000"
-fi
-
-URL="${HOST}/submit"
+URL="${1:-http://localhost:8000}/submit"
 
 curl -v -H 'Host: crash-reports' \
      -F 'uuid=a448814e-16dd-45fb-b7dd-b0b522161010' \
